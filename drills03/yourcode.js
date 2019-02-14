432d035766e2c48cf44044c01b25b7a2b454b49b
Eric George <geric738@aol.com>
2019-02-13 18:19:43 -0800

    built getPath function, and about half of getPathParts function

--- drills03/yourcode.js
+++ drills03/yourcode.js
@@ -1,11 +1,70 @@
 
 
-function getPath(){
+function getPath(string){
 
+    var array = [];
+    var directoryName = "";
+
+    for (var stringIndex=0; stringIndex<string.length; stringIndex++) {
+
+        if (string[stringIndex] !== "/") {
+            directoryName+=string[stringIndex];
+        }  else {
+            array.push(directoryName);
+            directoryName = "";
+        }
+
+        if (stringIndex === (string.length-1)) {
+            array.push(directoryName);
+        }
+
+    }
+
+    return array;
 }
 
-function getPathParts(){
+function getPathParts(string){
 
+    var pathObj = {};
+
+
+    //create protocol string and add it to the object 
+    var protocolString = "";
+    var protocolIndex = 0;
+    var colon = false;
+    do {
+
+        if (string[protocolIndex] !== ":") {
+        protocolString+=string[protocolIndex];
+        protocolIndex++;
+        } else {
+            colon = true;
+        }
+
+    } while (!colon);
+    pathObj.protocol = protocolString;
+
+
+    //find the position of the second forward slash, right before the host
+    var slashIndex = 0;
+    var slashCounter = 0;
+    var secondForwardSlash;
+    do {
+        if (string[slashIndex] !== "/") {
+            slashIndex++;
+        } else {
+            slashCounter++;
+            if (slashCounter<2) {
+                slashIndex++;
+            }
+        }
+    } while(slashCounter !== 2);
+    //slashIndex now holds the position of the second forward slash
+    //create host string and add it to the object
+
+    
+
+    return pathObj;
 }
 
 function getCapitalCount(){
