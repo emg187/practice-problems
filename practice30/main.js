1484fb7043c10043faee3f37694e1d068b66ebd3
Eric George <geric738@aol.com>
2019-02-19 11:58:55 -0800

    built 2nd versions of doMath, randomRange, and comboMath functions

--- practice30/main.js
+++ practice30/main.js
@@ -1,11 +1,27 @@
 function random_range(start_num, end_num){
 
     var result;
-    if (typeof(start_num) === "object" && typeof(end_num === "undefined")){
+    
+    if (typeof(start_num) === "object" && typeof(end_num) === "undefined"){
     var randomIndex = Math.floor(Math.random()*start_num.length);
     result = start_num[randomIndex];
     } else {
     result = Math.floor(Math.random()*end_num)+start_num;
+    }
+
+    return result;
+}
+
+
+function random_range2(start_num, end_num){
+
+    var result;
+
+    if (Array.isArray(start_num) && typeof(end_num) === "undefined"){
+        var randomIndex = Math.floor(Math.random()*start_num.length);
+        result = start_num[randomIndex];
+    } else {
+        result = Math.floor(Math.random()*end_num)+start_num;
     }
 
     return result;
